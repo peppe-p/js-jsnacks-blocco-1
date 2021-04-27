748592c6b742001886a2f6422e66b2dd807176b0
peppe-p <ombrachiara96@gmail.com>
2021-04-27 10:55:48 +0200

    Complete

---
+++ main.js
@@ -0,0 +1,12 @@
+//L’utente inserisce due parole in successione, con due prompt.
+var word1 = prompt("Inserisci la 1a parola");
+var word2 = prompt("Inserisci la 2a parola");
+
+//Il software stampa prima la parola più corta, poi la parola più lunga.
+if (word1.length < word2.length) {
+    console.log(word1 + " " + word2);
+} else if (word2.length < word1.length) {
+    console.log(word2 + " " + word1);
+} else {
+    console.log("Le parole hanno la stessa lunghezza!");
+}
