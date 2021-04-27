3ee7f9ca45e933ad1a82f5486676c0ac5a421649
peppe-p <ombrachiara96@gmail.com>
2021-04-27 11:54:43 +0200

    Complete JSnack 3

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+/* // ##### JSNACK 2 #####
 //L’utente inserisce due parole in successione, con due prompt.
 var word1 = prompt("Inserisci la 1a parola");
 var word2 = prompt("Inserisci la 2a parola");
@@ -10,3 +12,18 @@ if (word1.length < word2.length) {
 } else {
     console.log("Le parole hanno la stessa lunghezza!");
 }
+ */
+
+
+// ##### JSNACK 3 #####
+var numeri = [];
+//Il software deve chiedere per 10 volte all’utente di inserire un numero.
+for (i = 0; i < 10; i++) {
+    numeri.push(parseInt(prompt("Inserisci un numero")));
+}
+//Il programma stampa la somma di tutti i numeri inseriti.
+var somma = 0;
+for (i = 0; i < numeri.length; i++) {
+    somma = somma + numeri[i];
+}
+console.log(somma);
