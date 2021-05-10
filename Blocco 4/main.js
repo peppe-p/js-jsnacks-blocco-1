4e52fb9f27b8f17fa962e68d10adccf3a038f42a
peppe-p <ombrachiara96@gmail.com>
2021-05-10 11:44:56 +0200

    JSnack 1B Blocco 4

--- Blocco 4/main.js
+++ Blocco 4/main.js
@@ -1,9 +1,13 @@
-/* 1A:
+/*JSnack 1A
 Creare un oggetto palla che abbia le seguenti proprietà.
 Nome = palla
 Peso = 10 */
-
 var palla = {
     nome: "Palla",
     peso = 10,
 }
+
+
+/*JSnack 1B
+Attraverso un prompt dare la possibilità all’utente di modificare il peso della palla. */
+palla.peso = parseInt(prompt("Quanto pesa la palla?"));
